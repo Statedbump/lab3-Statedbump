8ec79605b214865b7c22629e9f7f3ad54418ce91
LuisCintron13 <luis.cintron16@upr.edu>
2017-02-07 20:54:42 -0400

    Centered and Size (200x200)

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,8 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Luis M. Cintron");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setSize(200, 200);
 		myFrame.setLocationRelativeTo(null);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
