0a84bacd0dbb063c83f966581493c8aa893922b0
LuisCintron13 <luis.cintron16@upr.edu>
2017-02-09 20:59:29 -0400

    Fix of JFrame

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,10 +4,11 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Luis M. Cintron");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setSize(200,200);
 		myFrame.setLocationRelativeTo(null);
-		myFrame.setSize(200, 200);
+		myFrame.setJMenuBar(null);
 		MyPanelClass myPanel = new MyPanelClass();
-        myFrame.getContentPane().add(myPanel);
+		myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 		
 		
