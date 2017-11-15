a06282997bc8da727f0ecd458805aeb5c1707d22
state <state@10.31.34.149>
2017-11-15 17:53:31 -0400

    This is my first commit

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,17 +1,19 @@
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
+
 	public static void main(String[] args) {
+
 		JFrame myFrame = new JFrame("Luis M. Cintron");
+
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setSize(300,200);
+
+		myFrame.setSize(200, 200);
+
 		myFrame.setLocationRelativeTo(null);
-		myFrame.setJMenuBar(null);
-		MyPanelClass myPanel = new MyPanelClass();
-		myFrame.getContentPane().add(myPanel);
+
 		myFrame.setVisible(true);
-		
-		
-		
+
 	}
+
 }
